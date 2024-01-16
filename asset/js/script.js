da54786a780342eb9e0f0fc547737d23868d1d8f
Thenorwood <144361668+Thenorwood@users.noreply.github.com>
2024-01-16 10:28:55 -0400

    commit

---
+++ asset/js/script.js
@@ -0,0 +1,8 @@
+//access the div with the id 'output'
+const outputDiv = document.getElementById('output');
+
+//log a message to the console
+console.log("Hello from the console");
+
+//display the same message on the HTML page
+outputDiv.innerText = "hello from the HTML!";
